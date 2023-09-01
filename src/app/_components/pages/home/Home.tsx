d0a6d3b2889786f3d0601e545171ccbfa673f333
Tsotne Darjania <tsotnedarjania1997@gmail.com>
2023-09-01 18:16:49 +0400

    test commit

--- src/app/_components/pages/home/Home.tsx
+++ src/app/_components/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./home.module.css";
 import AuthenticationForm from "./components/authenticationForm/AuthenticationForm";
-
+//test
 const Home = () => {
   return (
     <div className={style.home}>
